a9b87b38fc20889828d16b870db705e2820cfd8c
Veronique Fick <77314948+Veroniique@users.noreply.github.com>
2024-04-29 22:38:51 -0700

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+'''
+29 April 2024
+Creating a base class with 2 subclasses.
+Learning how attributes and methods work
+from classes
+'''
+
+#base class - Animal
+class Animal:
+   def __init__(self, name, age):
+       self.name = name
+       self.age = age
+
