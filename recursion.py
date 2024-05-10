f35b88306962be4ba19934dfc3a07250238a9b65
Veronique Fick <77314948+Veroniique@users.noreply.github.com>
2024-05-10 16:26:03 -0700

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,15 @@
+'''
+This program calculates the sum of a list of
+integers using recursion. A test function
+will verify the recursive implementation works
+'''
+
+#gets sum of list of integers
+def recursive_sum(list):
+    if not list:
+        return 0
+    else:
+        return list[0] + recursive_sum(list[1:])
+
+num_list = [3, 5, 8, 10, 15]
+print(recursive_sum(num_list))
